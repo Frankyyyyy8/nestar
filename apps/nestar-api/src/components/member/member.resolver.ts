9566803ccf37506a236520a74f7e8108fedf1d63
frank <farruxvloging@gmail.com>
2026-09-02 22:45:44 +0900

    feat: develop likeTargetMember GraphQL

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -76,6 +76,17 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: likeTargetMember');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
+
 	/**  ADMIN **/
 
 	@Roles(MemberType.ADMIN)
@@ -151,7 +162,7 @@ export class MemberResolver {
 						.on('error', () => reject(false));
 				});
 				if (!result) throw new Error(Message.UPLOAD_FAILED);
-               //@ts-ignore
+				//@ts-ignore
 				uploadedImages[index] = url;
 			} catch (err) {
 				console.log('Error, file missing!');
