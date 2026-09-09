20a17e00762e733e3b07410176f74e44fe1d6432
frank <farruxvloging@gmail.com>
2026-09-09 18:50:31 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers logic
    qa
    
    qa

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,9 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_PROPERTIES, BATCH_TOP_AGENTS } from './libs/config';
 
 @Controller()
 export class BatchController {
+	private logger: Logger = new Logger('BatchController');
+
 	constructor(private readonly BatchService: BatchService) {}
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+
+  @Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+  public async batchRollback() {
+    try {
+      this.logger['context'] = BATCH_ROLLBACK;
+      this.logger.debug('EXECUTED!')
+      await this.BatchService.batchRollback(); 
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+   @Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+  public async batchTopProperties() {
+    try {
+      this.logger['context'] = BATCH_TOP_PROPERTIES;
+      this.logger.debug('EXECUTED!')
+       await this.BatchService.batchProperties();
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+   @Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+  public async batchTopAgents() {
+    try {
+      this.logger['context'] = BATCH_TOP_AGENTS;
+      this.logger.debug('EXECUTED!')
+       await this.BatchService.batchAgents();
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
+
+	/*
+	@Interval(1000)
+	handleInterval() {
+		this.logger.debug('INTERVAL TEST');
+	}
+  */
 
 	@Get()
 	getHello(): string {
